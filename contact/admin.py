9c802acb086ae5e8fc8c52cdfcea41eaaee6f738
Gustavo Neves De Faria <72500182@aluno.faculdadecotemig.br>
2025-12-22 22:31:55 -0300

    Adaptando área do admin do django

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,9 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id','first_name', 'last_name', 
+    'phone', 'email', 'created_date')
+    ordering = ('id',)
+    #list_filter = ('created_date',)
+    search_fields = ('id','first_name', 'last_name', 'phone', 'email')
+    list_per_page = 10
